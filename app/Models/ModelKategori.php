8cf1a598638a0f6a2f9adfc17bb28b2821ff3cc2
azkifairuz <azkiajmal@gmail.com>
2023-06-22 00:11:17 +0700

    feat: get all kategori produk

--- app/Models/ModelKategori.php
+++ app/Models/ModelKategori.php
@@ -6,36 +6,8 @@ use CodeIgniter\Model;
 
 class ModelKategori extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'modelkategoris';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    public function getDataSatuan(){
+        $query = $this->db->query("SELECT * FROM kat_produk");
+            return $query->getResult();
+     }
 }
