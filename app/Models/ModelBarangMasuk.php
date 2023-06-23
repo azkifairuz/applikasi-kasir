eedfd2bdcb1b96cddd9380f7a0582dc222899392
azkifairuz <azkiajmal@gmail.com>
2023-06-23 22:11:18 +0700

    model getdata barang masuk

---
+++ app/Models/ModelBarangMasuk.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelBarangMasuk extends Model
+{
+    public function getDataProduk()
+    {
+        $query = $this->db->query("SELECT a.id_stok_masuk,no_faktur,a.tgl_masuk,a.jml_barang,a.harga_beli,s.nm_supplier,p.nm_produk FROM `barang_masuk` as a INNER JOIN produk as p ON a.id_produk = p.id_produk INNER JOIN supplier as s on a.id_supplier = s.id_supplier ");
+        return $query->getResult();
+    }
+    public function addProduk($data)
+    {
+        $query = $this->db->table('produk')
+            ->insert($data);
+        return $query;
+    }
+
+}
