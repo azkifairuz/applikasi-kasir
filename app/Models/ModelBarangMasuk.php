636d49b36806c7e92099a9eae20f4b484cfc63be
azkifairuz <azkiajmal@gmail.com>
2023-06-23 22:26:12 +0700

    feat:v_barangmasuk

--- app/Models/ModelBarangMasuk.php
+++ app/Models/ModelBarangMasuk.php
@@ -6,12 +6,12 @@ use CodeIgniter\Model;
 
 class ModelBarangMasuk extends Model
 {
-    public function getDataProduk()
+    public function getDataBarangMasuk()
     {
         $query = $this->db->query("SELECT a.id_stok_masuk,no_faktur,a.tgl_masuk,a.jml_barang,a.harga_beli,s.nm_supplier,p.nm_produk FROM `barang_masuk` as a INNER JOIN produk as p ON a.id_produk = p.id_produk INNER JOIN supplier as s on a.id_supplier = s.id_supplier ");
         return $query->getResult();
     }
-    public function addProduk($data)
+    public function addDataBarangMasuk($data)
     {
         $query = $this->db->table('produk')
             ->insert($data);
---
+++ app/Views/admin/v_barangMasuk.php
@@ -0,0 +1,119 @@
+<?php echo view('layout/header'); ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Barang Masuk</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Data Barang</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <?php
+      if (session()->get('success')) {
+        ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>
+            <?php echo session()->getFlashdata('success'); ?>
+          </strong>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <?php
+      }
+      ?>
+      <div class="row">
+        <div class="col-12">
+
+          <div class="card">
+            <div class="card-header row d-flex justify-content-between w-100">
+              <h3 class="card-title col-10">Barang Masuk</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="example3" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>nama produk</th>
+                    <th>stok</th>
+                    <th>satuan</th>
+                    <th>kategori</th>
+                    <th>deskripsi</th>
+                    <th>supplier</th>
+                    <th>harga beli</th>
+                    <th>harga jual</th>
+                    <th class="text-center">aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $no = 1;
+                  foreach ($showData as $row):
+                    ?>
+                    <tr>
+                      <td>
+                        <?php echo $no; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->nm_produk; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->stok; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->nm_satuan; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->nm_kategori; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->deskripsi; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->nm_supplier; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->harga_beli; ?>
+                      </td>
+                      <td>
+                        <?php echo $row->harga_jual; ?>
+                      </td>
+                      <td class="text-center">
+                        <a class="btn btn-success px-4 p-2"
+                          href="<?php echo base_url('produk/detailProduk/' . $row->id_produk); ?>">
+                          Edit
+                        </a>
+                      </td>
+                    </tr>
+                    <?php $no++; endforeach; ?>
+                </tbody>
+
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<?php echo view('layout/footer'); ?>
